a4ddddd347c045d594eefa619bed9f51317d2bef
Andrej Škvarenina <andrejskvrn@gmail.com>
2024-06-02 13:57:06 +0200

    kinde auth

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,10 +3,13 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
 import { ArrowRight } from "lucide-react";
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
-const Navbar = () => {
-  const user = undefined;
-  const isAdmin = false;
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
+
+  const isAdmin = user?.email === process.env.ADMIN_EMAIL
 
   return (
     <nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
@@ -30,7 +33,7 @@ const Navbar = () => {
                 </Link>
                 {isAdmin ? (
                   <Link
-                    href='/api/auth/logout'
+                    href='/dashboard'
                     className={buttonVariants({
                       size: "sm",
                       variant: "ghost",
@@ -62,7 +65,7 @@ const Navbar = () => {
                   Sign Up
                 </Link>
                 <Link
-                    href='/configure/upload'
+                    href='/api/auth/login'
                     className={buttonVariants({
                       size: "sm",
                       variant: "ghost",
